68083e43f88f8594f606b75c17f4193409207e83
YusukeMimakoya <83491074+YusukeMimakoya@users.noreply.github.com>
2022-12-26 13:35:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def inference(image, background_enhance, face_upsample, upscale, codeformer_fide
         face_size=512,
         crop_ratio=(1, 1),
         det_model=detection_model,
-        save_ext="png",
+        save_ext="pgm",
         use_parse=True,
         device=device,
     )
